6d06566768262f2b4cfde0f7251b317134362888
adrianaVilar <adriana.correa@outlook.com>
2022-05-17 19:35:53 -0300

    Quase um Hello World

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("adriana");
+console.log('adriana');
+console.log(`adriana`);
+
+console.log(30, 05, "adriana de noovo");
+
+console.log('Meu nome é "Adriana". Estou aprendendo JS às', 7, 'da noite.');
