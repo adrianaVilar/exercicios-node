d25e0a8ea8df764908d6e547d7a4d07a7d5c95af
adrianaVilar <adriana.correa@outlook.com>
2022-05-18 11:16:56 -0300

    aula 2

---
+++ aula5/js/index.js
@@ -0,0 +1,12 @@
+console.log("adriana");
+console.log("adriana");
+console.log(`adriana`);
+
+console.log(30, 05, "adriana de noovo");
+
+console.log('Meu nome é "Adriana". Estou aprendendo JS às', 7, "da noite.");
+
+//Um comentário
+console.log("Olá"); //Só aparece no console!
+
+// alert("Oi"); -> só funciona no navegador, como uma janela block
