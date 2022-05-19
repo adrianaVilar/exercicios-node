e7dfd64a3dbf30d73f80737f392fde3bf024794d
adrianaVilar <adriana.correa@outlook.com>
2022-05-19 13:38:22 -0300

    Exercicio calculadora

---
+++ aula11/js/script.js
@@ -0,0 +1,9 @@
+alert("Bem vindo a calculadora!");
+
+let n1 = prompt("Digite um número:");
+let n2 = prompt("Digite outro número:");
+
+n1 = Number(n1);
+n2 = Number(n2);
+
+alert(`O resultado da soma destes dois números é: ${n1 + n2}`);
