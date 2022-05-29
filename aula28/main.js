668f7590ef5f1c7eb322bf83cbc5843da65e2ccd
adrianaVilar <adriana.correa@outlook.com>
2022-05-29 19:17:26 -0300

    Aula for in com array e com objeto

--- aula28/main.js
+++ aula28/main.js
@@ -1,5 +1,19 @@
+//Com array
+
 const frutas = ["Maçã", "Pêra", "Uva"];
 
 for (let i in frutas) {
   console.log(i, "->", frutas[i]);
+}
+
+//Com objeto
+
+const pessoa = {
+  nome: "Adriana",
+  sobrenome: "Vilar",
+  idade: 28,
+};
+
+for (let i in pessoa) {
+  console.log(i, ":", pessoa[i]);
 }
