5b422b1971e29a5ad2226f175340a41fd2c00b13
adrianaVilar <adriana.correa@outlook.com>
2022-05-29 20:16:11 -0300

    Aula do while

--- aula31/main.js
+++ aula31/main.js
@@ -14,7 +14,12 @@ const min = 1;
 const max = 50;
 let rand = random(min, max);
 
-while (rand !== 10) {
+// while (rand !== 10) {
+//   rand = random(min, max);
+//   console.log(rand);
+// }
+
+do {
   rand = random(min, max);
   console.log(rand);
-}
+} while (rand !== 10);
