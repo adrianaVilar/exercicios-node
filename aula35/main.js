be3ce0ac9ccd5957d6b606ba7f7031631e28e0d0
adrianaVilar <adriana.correa@outlook.com>
2022-05-30 16:54:22 -0300

    Exercício FizzBuzz, com teste

---
+++ aula35/main.js
@@ -0,0 +1,18 @@
+function numero(num) {
+  const divisivelPorTres = (num) => (num % 3 === 0 ? "Fizz" : "");
+  const divisivelPorCinco = (num) => (num % 5 === 0 ? "Buzz" : "");
+
+  if (divisivelPorTres(num) === "" && divisivelPorCinco(num) === "") {
+    return num;
+  }
+  if (isNaN(num)) {
+    return num;
+  }
+
+  return divisivelPorTres(num) + divisivelPorCinco(num);
+}
+console.log(numero(21), "deve retornar Fizz");
+console.log(numero(10), "deve retornar Buzz");
+console.log(numero(15), "deve retornar FizzBuzz");
+console.log(numero(7), "deve retornar 7");
+console.log(numero("qwe"), "deve retornar qwe");
