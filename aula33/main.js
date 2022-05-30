ab7240147266792e8570fae635265c202d28c4b3
adrianaVilar <adriana.correa@outlook.com>
2022-05-29 21:08:42 -0300

    Exercicio retornar maior valor

--- aula33/main.js
+++ aula33/main.js
@@ -1,15 +1,13 @@
 //Função que recebe 2 números e retorna o maior:
 
 function getLarger(n1, n2) {
-  if (n1 > n2) {
-    console.log(n1);
-  } else if (n2 > n1) {
-    console.log(n2);
-  } else if (n1 === n2) {
-    console.log("Os números são iguais.");
+  if (n1 > n2) return n1;
+  if (n2 > n1) return n2;
+  if (n1 === n2) {
+    return "Os números são iguais.";
   } else {
-    console.log("Digite um número válido");
+    return "Digite um número válido";
   }
 }
 
-getLarger(3, "asd");
+console.log(getLarger(3, 3));
