1561bcad75be2bf4d6caf38c8853f69ed69748c7
adrianaVilar <adriana.correa@outlook.com>
2022-05-31 03:42:31 -0300

    Desafio timer

---
+++ aula39 copy/assets/js/script.js
@@ -0,0 +1,42 @@
+const relogio = document.querySelector(".relogio");
+const iniciar = document.querySelector(".iniciar");
+const pausar = document.querySelector(".pausar");
+const zerar = document.querySelector(".zerar");
+let segundos = 0;
+let timer;
+
+function criaHora(segundos) {
+  let hora = Math.trunc(segundos / 3600);
+  let minuto = Math.trunc(segundos / 60);
+  let segundo = Math.trunc(segundos % 60);
+
+  function zeroAEsquerda(num) {
+    return num >= 10 ? num : `0${num}`;
+  }
+
+  return `<p>${zeroAEsquerda(hora)}:${zeroAEsquerda(minuto)}:${zeroAEsquerda(
+    segundo
+  )}</p>`;
+}
+
+iniciar.addEventListener("click", function (event) {
+  timer = setInterval(function () {
+    segundos++;
+    relogio.innerHTML = criaHora(segundos);
+  }, 1000);
+  event.preventDefault();
+  relogio.style.color = "#000000";
+});
+
+pausar.addEventListener("click", function (event) {
+  event.preventDefault;
+  clearInterval(timer);
+  relogio.style.color = "#FF0000";
+});
+
+zerar.addEventListener("click", function () {
+  clearInterval(timer);
+  segundos = 0;
+  relogio.innerHTML = criaHora(0);
+  relogio.style.color = "#000000";
+});
