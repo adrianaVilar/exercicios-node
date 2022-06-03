8096ebcd1e0dbad064889bbbd55c201e2d61c6db
adrianaVilar <adriana.correa@outlook.com>
2022-06-03 12:23:13 -0300

    Filter

--- aula55/main.js
+++ aula55/main.js
@@ -1,13 +1,27 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-function callbackFilter(valor, indice, array) {
+//Separando a função e com verificação
+function callbackFilter(valor) {
   return valor > 10;
 }
 
 const numerosFiltrados1 = numeros.filter(callbackFilter);
 console.log(numerosFiltrados1);
 
+//Com arrow function
 callback2 = (valor) => valor > 10;
 
 const numerosFiltrados2 = numeros.filter(callback2);
 console.log(numerosFiltrados2);
+
+//Com função anônima
+const numerosFiltrados3 = numeros.filter(function (valor) {
+  return valor > 10;
+});
+
+console.log(numerosFiltrados3);
+
+//Com arrow function2
+const numerosFiltrados4 = numeros.filter((valor) => valor > 10);
+
+console.log(numerosFiltrados4);
