b0043be60288601f57a97efa3b8628c61bcf76da
adrianaVilar <adriana.correa@outlook.com>
2022-06-02 22:30:28 -0300

    Revisão de array

---
+++ aula51/main.js
@@ -0,0 +1,16 @@
+// const nomes = ["Adri", "Erick", "Lu"];
+// const novo = [...nomes];
+
+// nomes.push("Jerry");
+// nomes[100] = "Adriana";
+
+// console.log(nomes);
+// console.log(novo);
+
+// const nome = "Adriana Corrêa Vilar";
+// const nomes = nome.split(" ");
+// console.log(nomes);
+
+const nomes = ["Adri", "Corrêa", "Vilar"];
+const nome = nomes.join(" - ");
+console.log(nome);
