b9334dc8327f32ea60faf37b47b96fee0829dbf3
adrianaVilar <adriana.correa@outlook.com>
2022-06-06 16:42:41 -0300

    object.freeze

--- aula65/main.js
+++ aula65/main.js
@@ -4,3 +4,10 @@ const produto2 = { ...produto, material: "alumínio" };
 console.log(produto);
 produto2.nome = "Xícara";
 console.log(produto2);
+
+Object.freeze(produto);
+produto.nome = "Prato"; // Essa alteração não vai ocorrer, por causa da linha 8
+produto2.nome = "Pires";
+
+console.log(produto);
+console.log(produto2);
