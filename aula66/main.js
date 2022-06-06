9c505d4bf8d0ba1b5f83a91e92d0e27b26aaf065
adrianaVilar <adriana.correa@outlook.com>
2022-06-06 16:56:20 -0300

    Object.values e Object.keys

--- aula66/main.js
+++ aula66/main.js
@@ -2,3 +2,6 @@ const produto = { nome: "Caneca", preco: 2.99 };
 const produto2 = { ...produto, material: "alumínio" };
 
 console.log(Object.getOwnPropertyDescriptors(produto, "nome", "preco"));
+
+console.log(Object.keys(produto));
+console.log(Object.values(produto));
