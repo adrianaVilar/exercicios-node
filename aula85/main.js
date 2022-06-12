ea0660cdab8bbfb8ee453ad6f69ec01b3568e12c
adrianaVilar <adriana.correa@outlook.com>
2022-06-12 18:27:02 -0300

    Promise.race

---
+++ aula85/main.js
@@ -0,0 +1,29 @@
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof msg !== "string") {
+        reject("Cai no ERRO");
+        return;
+      }
+
+      resolve(msg.toUpperCase() + " - Passei na promise");
+      return;
+    }, tempo);
+  });
+}
+
+const promises = [
+  esperaAi("Promise 1", 3000),
+  esperaAi("Promise 2", 500),
+  esperaAi("Promise 3", 1000),
+];
+
+Promise.race(promises)
+  .then((valor) => console.log(valor))
+  .catch((erro) => console.log(erro));
