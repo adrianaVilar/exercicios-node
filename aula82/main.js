f41824347bc5d7b0e0feb4033cc12d096364b316
adrianaVilar <adriana.correa@outlook.com>
2022-06-12 18:08:34 -0300

    Promise.resolve

---
+++ aula82/main.js
@@ -0,0 +1,36 @@
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof msg !== "string") {
+        reject("Cai no ERRO");
+        return;
+      }
+
+      resolve(msg.toUpperCase() + " - Passei na promise");
+      return;
+    }, tempo);
+  });
+}
+
+function baixaPagina() {
+  const emCache = false;
+  //   const emCache = true;
+
+  if (emCache) {
+    return Promise.resolve("Página em cache");
+  } else {
+    return esperaAi("Baixei a página", 3000);
+  }
+}
+
+baixaPagina()
+  .then((dadosPagina) => {
+    console.log(dadosPagina);
+  })
+  .catch((e) => console.log(e));
