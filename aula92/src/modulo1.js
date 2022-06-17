a74ccbaf799eb49d32d6fb3ad3f1f804b1903797
adrianaVilar <adriana.correa@outlook.com>
2022-06-17 00:18:33 -0300

    ES6 Modules: export e import

---
+++ aula92/src/modulo1.js
@@ -0,0 +1,15 @@
+//Exportar
+
+export const nome = "Adriana";
+export const sobrenome = "Vilar";
+export const idade = 28;
+
+export function soma(x, y) {
+  return x + y;
+}
+
+export class Pessoa {
+  constructor(nome, sobrenome) {
+    this.nome = nome;
+  }
+}
