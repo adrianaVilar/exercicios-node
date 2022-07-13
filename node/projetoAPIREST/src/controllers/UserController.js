e8c4694d8d033d200ba19316df3bdd936004368f
Adriana Vilar <adriana.correa@outlook.com>
2022-07-13 20:01:34 -0300

    migrations, sequelize e crud

---
+++ node/projetoAPIREST/src/controllers/UserController.js
@@ -0,0 +1,90 @@
+import User from "../models/User";
+
+class UserController {
+  async store(req, res) {
+    try {
+      const novoUser = await User.create(req.body);
+      return res.json(novoUser);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      // retorna nulo pq se houver erro, foi o código que quebrou e ñ a pessoa que digitou errado
+      return res.json(null);
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      // retorna nulo pq se houver erro, foi o código que quebrou e ñ a pessoa que digitou errado
+      return res.json(null);
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ["ID não enviado."],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ["O usuário não existe."],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ["ID não enviado."],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ["O usuário não existe."],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+}
+
+export default new UserController();
