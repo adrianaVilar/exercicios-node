cf2b73a11d542b6294b41c2574de94521ffbc5a6
Adriana Vilar <adriana.correa@outlook.com>
2022-07-13 22:54:00 -0300

    removendo rotas indesejadas

--- node/projetoAPIREST/src/routes/user.js
+++ node/projetoAPIREST/src/routes/user.js
@@ -5,10 +5,14 @@ import loginRequired from "../middlewares/loginRequired";
 
 const router = new Router();
 
-router.post("/", userController.store);
-router.get("/", loginRequired, userController.index);
-router.get("/:id", userController.show);
-router.put("/:id", userController.update);
-router.delete("/:id", userController.delete);
+// Rotas que complemantam o CRUD, mas no mundo real não usaria
+// router.get("/", loginRequired, userController.index);
+// router.get("/:id", userController.show);
+// router.put("/:id", userController.update); -> Dessa forma qlqr usuario poderia atualizar
+// router.delete("/:id", userController.delete); -> Dessa forma qlqr user poderia deletar
+
+router.post("/", userController.store); // Esse não precisa de login, pq o usuario precisa acessar para criar conta
+router.put("/", loginRequired, userController.update); // Precisa de login
+router.delete("/", loginRequired, userController.delete); // Precisa de login
 
 export default router;
