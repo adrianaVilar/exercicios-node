49131a56f230cfc805a41bbbb5396b1f9ff1f612
Adriana Vilar <adriana.correa@outlook.com>
2022-07-27 19:20:49 -0300

    editando itens da lista

--- react/listatarefas/src/components/Main.js
+++ react/listatarefas/src/components/Main.js
@@ -11,12 +11,13 @@ import "./Main.css";
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    tarefas: []
+    tarefas: [],
+    index: -1,
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     // Trim = remove espaços desnecessários
     novaTarefa = novaTarefa.trim();
@@ -25,10 +26,19 @@ export default class Main extends Component {
 
     const novasTarefas = [...tarefas];
 
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    });
+    if(index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: "",
+      });
+    }else {
+      novasTarefas[index] = novaTarefa;
 
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
   }
 
   handleChange = (e) => {
@@ -39,7 +49,13 @@ export default class Main extends Component {
 
 
   handleEdit = (e, index) => {
-console.log("Edit", index);
+    const { tarefas } = this.state;
+
+    this.setState({
+        index,
+        novaTarefa: tarefas[index],
+      });
+
 }
 
 handleDelete = (e, index) => {
