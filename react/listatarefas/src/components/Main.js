96f3b424ed47990f5150f9aa288bcc9edd755758
Adriana Vilar <adriana.correa@outlook.com>
2022-07-26 21:39:48 -0300

    adicionando tarefas

--- react/listatarefas/src/components/Main.js
+++ react/listatarefas/src/components/Main.js
@@ -11,12 +11,25 @@ import "./Main.css";
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    tarefas: [
-      'Fazer café',
-      'Fazer pão',
-      'Estudar',
-    ]
+    tarefas: []
   };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    // Trim = remove espaços desnecessários
+    novaTarefa = novaTarefa.trim();
+
+    if(tarefas.indexOf(novaTarefa) != -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    this.setState({
+      tarefas: [...novasTarefas, novaTarefa],
+    });
+
+  }
 
   handleChange = (e) => {
     this.setState({
@@ -31,7 +44,7 @@ export default class Main extends Component {
   return (
     <div className="main">
       <h1>Lista de tarefas</h1>
-      <form action="#" className="form">
+      <form onSubmit={this.handleSubmit} action="#" className="form">
       <input onChange={this.handleChange} type="text" value={novaTarefa} />
       <button type="submit">
         <FaPlus />
@@ -42,10 +55,10 @@ export default class Main extends Component {
       {tarefas.map(tarefas => (
         <li key={tarefas}>
           {tarefas}
-          <div>
+          <span>
 <FaEdit className="edit"/>
 <FaWindowClose className="delete"/>
-            </div></li>
+            </span></li>
       ))}
     </ul>
 
