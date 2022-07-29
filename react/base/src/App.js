81cc1a45e1fc9609affa8f9caeb2a5bceb10a4bc
Adriana Vilar <adriana.correa@outlook.com>
2022-07-29 01:32:12 -0300

    adicionado header, routes e pag404

--- react/base/src/App.js
+++ react/base/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routed from './routes';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
+      <Routed />
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   );
 }
 
