30215b4c46e7d5ea1dd12584f79a75e55968b7b1
Adriana Vilar <adriana.correa@outlook.com>
2022-08-01 15:15:38 -0300

    configurado axios para a url da api

--- react/base/src/pages/Login/index.js
+++ react/base/src/pages/Login/index.js
@@ -3,7 +3,21 @@ import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  /* Se colocar algo entre os colchetes, cada vez que ele mudar, a função vai ser executada
+   * Se deixar só algo entre as chaves, só vai executar quando o componente for montado
+   */
+  React.useEffect(() => {
+    async function getData() {
+      const response = await axios.get('/events');
+      console.log(response);
+    }
+
+    getData();
+  }, []);
+
   return (
     <Container>
       <Title isRed={false}>
